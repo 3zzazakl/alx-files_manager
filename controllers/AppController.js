d9c254baa29aeb4f51f938972364912161105b77
3zzazakl <vanzekovichgr@gmail.com>
2024-12-13 17:42:04 +0200

    fixing db

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,21 +1,26 @@
+import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
   static async getStatus(req, res) {
     try {
       const redisStatus = redisClient.isAlive();
-      return res.status(200).json({ redis: redisStatus });
+      const dbStatus = await dbClient.isAlive();
+      return res.status(200).json({ redis: redisStatus, db: dbStatus });
     } catch (error) {
+      console.error('Error getting status:', error);
       return res.status(500).json({ error: 'An error occurred while checking Redis status' });
     }
   }
 
   static async getStats(req, res) {
     try {
-      const value = await redisClient.get('someKey');
-      return res.status(200).json({ value });
+      const users = await dbClient.nbUsers();
+      const files = await dbClient.nbFiles();
+      return res.status(200).json({ users, files });
     } catch (error) {
-      return res.status(500).json({ error: 'An error occurred while getting Redis stats' });
+      console.error('Error getting stats:', error);
+      return res.status(500).json({ error: 'An error occurred while getting stats' });
     }
   }
 }
