95ed3468bcb5fa5e365342083a835c1a68703c99
3zzazakl <vanzekovichgr@gmail.com>
2024-12-14 04:28:10 +0200

    setting user

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -5,7 +5,6 @@ import userUtils from '../utils/user';
 import fileUtils from '../utils/file';
 import basicUtils from '../utils/basic';
 
-
 const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
 
 const fileQueue = new Queue('fileQueue');
@@ -14,16 +13,22 @@ class FilesController {
   static async postUpload(request, response) {
     const { userId } = await userUtils.getUserIdAndKey(request);
 
-    if (!basicUtils.isValidId(userId)) { return response.status(401).send({ error: 'Unauthorized' }); }
+    if (!basicUtils.isValidId(userId)) {
+      return response.status(401).send({ error: 'Unauthorized' });
+    }
 
     if (!userId && request.body.type === 'image') {
       await fileQueue.add({});
     }
-    const user = await userUtils.getUser({ _id: ObjectId(userId) });
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
 
     if (!user) { return response.status(401).send({ error: 'Unauthorized' }); }
 
-    const { error: validationError, fileParams } = await fileUtils.validateBody(request);
+    const { error: validationError, fileParams } = await fileUtils.validateBody(
+      request,
+    );
 
     if (validationError) { return response.status(400).send({ error: validationError }); }
 
@@ -31,10 +36,15 @@ class FilesController {
       return response.status(400).send({ error: 'Parent not found' });
     }
 
-    const { error, code, newFile } = await fileUtils.saveFile(userId, fileParams, FOLDER_PATH);
+    const { error, code, newFile } = await fileUtils.saveFile(
+      userId,
+      fileParams,
+      FOLDER_PATH,
+    );
 
     if (error) {
       if (response.body.type === 'image') await fileQueue.add({ userId });
+      return response.status(code).send(error);
     }
 
     if (fileParams.type === 'image') {
@@ -43,72 +53,91 @@ class FilesController {
         userId: newFile.userId.toString(),
       });
     }
+
     return response.status(201).send(newFile);
   }
 
   static async getShow(request, response) {
     const fileId = request.params.id;
     const { userId } = await userUtils.getUserIdAndKey(request);
-    const user = await userUtils.getUser({ _id: ObjectId(userId) });
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
 
     if (!user) { return response.status(401).send({ error: 'Unauthorized' }); }
 
     if (!basicUtils.isValidId(fileId) || !basicUtils.isValidId(userId)) { return response.status(404).send({ error: 'Not found' }); }
 
-    const result = await fileUtils.getFile({ _id: ObjectId(fileId), userId: ObjectId(userId) });
+    const result = await fileUtils.getFile({
+      _id: ObjectId(fileId),
+      userId: ObjectId(userId),
+    });
 
     if (!result) { return response.status(404).send({ error: 'Not found' }); }
 
+    const file = fileUtils.processFile(result);
     return response.status(200).send(file);
   }
 
   static async getIndex(request, response) {
     const { userId } = await userUtils.getUserIdAndKey(request);
-    const user = await userUtils.getUser({ _id: ObjectId(userId) });
+
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
     if (!user) { return response.status(401).send({ error: 'Unauthorized' }); }
 
     let parentId = request.query.parentId || '0';
-    if (parent === '0') parentId = 0;
+
+    if (parentId === '0') parentId = 0;
 
     let page = Number(request.query.page) || 0;
+
     if (Number.isNaN(page)) page = 0;
 
     if (parentId !== 0 && parentId !== '0') {
-      if (!basicUtils.isValidId(parentId)) { return response.status(401).send({ error: 'Unauthorized' }); }
+      if (!basicUtils.isValidId(parentId)) {
+        return response.status(401).send({ error: 'Unauthorized' });
+      }
 
       parentId = ObjectId(parentId);
 
-      const folder = await fileUtils.getFile({ _id: parentId });
-
-      if (!folder || folder.type !== 'folder') { return response.status(200).send([]); }
-
-      const pipeline = [
-        { $match: { parentId: ObjectId(parentId) } },
-        { $skip: page * 20 },
-        { $limit: 20 },
-      ];
-    
-      const fileCursor = await fileUtils.getFilesOfParentId(pipeline);
-      
-      const fileList = [];
-      await fileCursor.forEach((doc) => {
-        const document = fileUtils.processFile(doc);
-        fileList.push(document);
+      const folder = await fileUtils.getFile({
+        _id: parentId,
       });
 
-      return response.status(200).send(fileList);
+      if (!folder || folder.type !== 'folder') { return response.status(200).send([]); }
     }
-    
-    static async putPublish(request, response) {
+
+    const pipeline = [
+      { $match: { parentId } },
+      { $skip: page * 20 },
+      {
+        $limit: 20,
+      },
+    ];
+
+    const fileCursor = await fileUtils.getFilesOfParentId(pipeline);
+
+    const fileList = [];
+    await fileCursor.forEach((doc) => {
+      const document = fileUtils.processFile(doc);
+      fileList.push(document);
+    });
+
+    return response.status(200).send(fileList);
+  }
+
+  static async putPublish(request, response) {
     const { error, code, updateFile } = await fileUtils.publishUnpublis(
       request,
-      true
+      true,
     );
 
     if (error) return response.status(code).send({ error });
     return response.status(code).send(updateFile);
   }
-  
+
   static async getFile(request, response) {
     const { userId } = await userUtils.getUserIdAndKey(request);
     const { id: fileId } = request.params;
@@ -125,7 +154,7 @@ class FilesController {
     if (file.type === 'folder') { return response.status(400).send({ error: "A folder doesn't have content" }); }
 
     const { error, code, data } = await fileUtils.getFileData(file, size);
-    
+
     if (error) return response.status(code).send({ error });
 
     const mimeType = mime.contentType(file.name);
@@ -136,4 +165,3 @@ class FilesController {
 }
 
 export default FilesController;
-
