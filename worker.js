61893a9e977672107d8db30161cac6352495098e
3zzazakl <vanzekovichgr@gmail.com>
2024-12-14 02:08:28 +0200

    setting worker

--- worker.js
+++ worker.js
@@ -1,41 +1,43 @@
 import Queue from 'bull';
 import { ObjectId } from 'mongodb';
 import { promises as fspromises } from 'fs';
-import fileUtils from '../models/File';
-import User from '../models/User';
+import fileUtils from '../utils/file';
+import userUtils from '../utils/user';
+import basicUtils from '../utils/basic';
 
-const Bull = require('bull');
 const imageThumbnail = require('image-thumbnail');
-const fs = require('fs');
-const path = require('path');
-const redis = require('redis');
 
-const fileQueue = new Bull('fileQueue');
+const fileQueue = new Queue('fileQueue');
+const userQueue = new Queue('userQueue');
 
 fileQueue.process(async (job) => {
-  const { userId, fileId } = job.data;
+  const { fileId, userId } = job.data;
 
   if (!fileId) throw new Error('Missing fileId');
   if (!userId) throw new Error('Missing userId');
 
-  const file = await File.findOne({ where: { id: fileId, userId } });
+  if (!basicUtils.isValidId(fileId) || !basicUtils.isValidId(userId)) throw new Error('File not found');
+
+  const file = await fileUtils.getFile({
+    _id: ObjectId(fileId),
+    userId: ObjectId(userId),
+  });
+
   if (!file) throw new Error('File not found');
 
-  const filePath = path.join(__dirname, 'uploads', file.name);
-  const sizes = [500, 250, 100];
+  const { localPath } = file;
+  const options = {};
+  const widths = [500, 250, 100];
 
-  for (const size of sizes) {
-    const thumbnail = await imageThumbnail(filePath, { width: size });
-    const thumbnailPath = `${filePath}_${size}`;
-    fs.writeFileSync(thumbnailPath, thumbnail);
-  }
-});
-
-const userQueue = new Bull('userQueue', {
-  redis: {
-    host: '127.0.0.1',
-    port: 6379,
-  },
+  widths.forEach(async (width) => {
+    options.width = width;
+    try {
+      const thumbnail = await imageThumbnail(localPath, options);
+      await fspromises.writeFile(`${localPath}_${width}`, thumbnail);
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
 });
 
 userQueue.process(async (job) => {
@@ -43,18 +45,9 @@ userQueue.process(async (job) => {
 
   if (!userId) throw new Error('Missing userId');
 
-  const user = await User.findById(userId);
+  if (!basicUtils.isValidId(userId)) throw new Error('User not found');
+
+  const user = await userUtils.getUser({ _id: ObjectId(userId) });
   if (!user) throw new Error('User not found');
-
   console.log(`Welcome ${user.email}!`);
-
-  return { message: 'Welcome email sent successfully!' };
 });
-
-userQueue.on('failed', (job, err) => {
-  console.error(`Job failed for user ${job.data.userId}: ${err.message}`);
-});
-
-module.exports = {
-  fileQueue,
-};
