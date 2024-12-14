062839550f32268035d073fcae239b4d2b58a924
3zzazakl <vanzekovichgr@gmail.com>
2024-12-14 02:50:37 +0200

    setting fielg

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import route from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(route);
+app.use(express.json());
+controllerRouting(app);
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
+
+export default app;
