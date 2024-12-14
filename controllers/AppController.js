8454e3c1f377e2b02767fb46b31ea1823f6629da
3zzazakl <vanzekovichgr@gmail.com>
2024-12-14 03:16:42 +0200

    setting fielg

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,26 +2,20 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 class AppController {
-  static async getStatus(req, res) {
-    try {
-      const redisStatus = redisClient.isAlive();
-      const dbStatus = await dbClient.isAlive();
-      return res.status(200).json({ redis: redisStatus, db: dbStatus });
-    } catch (error) {
-      console.error('Error getting status:', error);
-      return res.status(500).json({ error: 'An error occurred while checking Redis status' });
-    }
+  static getStatus(request, response) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    response.status(200).json(status);
   }
 
-  static async getStats(req, res) {
-    try {
-      const users = await dbClient.nbUsers();
-      const files = await dbClient.nbFiles();
-      return res.status(200).json({ users, files });
-    } catch (error) {
-      console.error('Error getting stats:', error);
-      return res.status(500).json({ error: 'An error occurred while getting stats' });
-    }
+  static async getStats(request, response) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    response.status(200).json(stats);
   }
 }
 
